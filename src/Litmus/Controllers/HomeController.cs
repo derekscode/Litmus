c799f487b8dda132502d28468e6113c0e712ada9
derekscode <derekscode@github.com>
2016-07-08 16:33:00 -0700

    got it building

--- src/Litmus/Controllers/HomeController.cs
+++ src/Litmus/Controllers/HomeController.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Mvc;
-using Microsoft.AspNet.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Litmus.Controllers
 {
